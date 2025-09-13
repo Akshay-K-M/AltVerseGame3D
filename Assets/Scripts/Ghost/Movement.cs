fca96e5c23de51d24baca9ff7737f20b8dff9ac4
Vedansh Patel <vcp23062006@gmail.com>
2025-09-13 14:34:17 +0530

    movement

--- Assets/Scripts/Ghost/Movement.cs
+++ Assets/Scripts/Ghost/Movement.cs
@@ -2,25 +2,52 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    Vector2 movement;
-    CharacterController controller;
-    public float speed;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float moveSpeed;
+    public float mouseSensitivity;
+
+    private CharacterController controller;
+
+    // rotation state
+    float pitch = 0f; // up/down rotation
+    float yaw = 0f;   // left/right rotation
+
     void Start()
     {
         controller = GetComponent<CharacterController>();
 
+        // Lock cursor in center of screen
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        movement.x = Input.GetAxis("Horizontal");
-        movement.y = Input.GetAxis("Vertical");
-        Vector3 direction = new Vector3(movement.x, 0, movement.y).normalized;
-        if(direction.magnitude >= 0.1f)
-        {
-            controller.Move(direction * Time.deltaTime * speed);
-        }
+        // HandleMouseLook();
+        HandleMovement();
+    }
+
+    void HandleMouseLook()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+        yaw += mouseX;
+        pitch -= mouseY; // subtract to invert controls like FPS
+        pitch = Mathf.Clamp(pitch, -89f, 89f); // prevent flipping upside down
+
+        // Apply rotation
+        transform.rotation = Quaternion.Euler(pitch, yaw, 0f);
+    }
+
+    void HandleMovement()
+    {
+        float x = Input.GetAxis("Horizontal"); // A/D
+        float z = Input.GetAxis("Vertical");   // W/S
+
+        // move only on the horizontal plane (ignore eyes.up)
+        Vector3 move = transform.right * x + transform.forward * z;
+        move.y = 0f; // ensure no vertical movement
+
+        controller.Move(move * moveSpeed * Time.deltaTime);
     }
 }
